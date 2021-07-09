caa4875a7cde324c4afefd0fc5340c84124038c3
Rami A <61546894+ralch22@users.noreply.github.com>
2021-07-09 21:56:52 +1000

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Layout from "@/components/layout";
+import CallToActionOne from "@/components/call-to-action-one";
+import Footer from "@/components/footer";
+import BlogHome from "@/components/blog-home";
+import ParallaxOne from "@/components/parallax-1";
+import ClientCarouselOne from "@/components/client-carousel-one";
+import TeamCarousel from "@/components/team-carousel";
+import FunfactOne from "@/components/funfact-one";
+import TrustedClient from "@/components/trusted-client";
+import PortfolioHome from "@/components/portfolio-home";
+import SubscribeForm from "@/components/subscribe-form";
+import ServiceTwo from "@/components/service-two";
+import AboutTwo from "@/components/about-two";
+import VideoTwo from "@/components/video-two";
+import HeaderOne from "@/components/header-one";
+import SearchContextProvider from "@/context/search-context";
+import MenuContextProvider from "@/context/menu-context";
+import SliderOne from "@/components/slider-one";
 
-export default function Home() {
+const HomeOne = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MenuContextProvider>
+      <SearchContextProvider>
+        <Layout PageTitle="Home One">
+          <HeaderOne />
+          <SliderOne />
+          <ServiceTwo />
+          <AboutTwo />
+          <VideoTwo />
+          <SubscribeForm />
+          <PortfolioHome />
+          <FunfactOne />
+          <TrustedClient />
+          <TeamCarousel />
+          <ClientCarouselOne />
+          <ParallaxOne />
+          <BlogHome />
+          <CallToActionOne extraClassName="ready" />
+          <Footer />
+        </Layout>
+      </SearchContextProvider>
+    </MenuContextProvider>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default HomeOne;
